c8794d67d5990a551599b7e7f2f07eb34704b625
Faviun <lightbringerlal@gmail.com>
2025-10-17 13:25:25 +0300

    vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import Pages from 'vite-plugin-pages';
-import path from 'path';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Pages from "vite-plugin-pages";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages()
-  ],
+  plugins: [react(), Pages()],
+  preview: {
+    host: true, // или 0.0.0.0
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    allowedHosts: ["metall-it-front-6.onrender.com"],
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
